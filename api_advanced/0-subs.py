a2581faf1df35559669bf804a8a8377627553c11
mukezw <mukezwa@gmail.com>
2024-07-06 18:35:31 +0200

    ref: re add 0-subs

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -4,15 +4,16 @@ function that queries the 'Reddit API' and returns the number of subscribers
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
     number of subscribers
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}  # avoid Too Many Requests error
 
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
